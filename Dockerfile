5849bae5bfaaebadb06ad77f7634b1f0ec37780c
Guillaume Allain <guillaume.allain@argos.co.uk>
2018-01-20 18:10:54 +0000

    use google storage for model

--- Dockerfile
+++ Dockerfile
@@ -3,10 +3,9 @@ FROM python:3.6-slim
 #RUN apt-get update && apt-get install -y python-opencv
 
 WORKDIR /app
-ADD ./*requirements.txt /app/
 RUN apt-get update && apt-get -y install libxext6 libglib2.0-0 libsm6 libxrender1
+ADD ./*requirements.txt /app/
 RUN pip install -r requirements.txt
-
 ADD . /app
 
 ENTRYPOINT ["gunicorn", "-b", ":8080", "app:app", "--pythonpath", "src,src/chessid", "--capture-output"]
--- src/chessid/annotator.py
+++ src/chessid/annotator.py
@@ -7,15 +7,32 @@ from PIL import ImageDraw
 from chessid import classifier
 from chessid import detection
 
+model_name = 'trained_non_lin_model_best.pth.tar'
+model_path = os.path.join(os.path.dirname(__file__), model_name)
+model = None
+
+
+def load_model():
+    if not os.path.exists(model_path):
+        from google.cloud import storage
+        client = storage.Client()
+        bucket = client.get_bucket('chess-id.appspot.com')
+        blob = bucket.get_blob(model_name)
+        blob.download_to_filename(model_path)
+
+    global model
+    model = classifier.ImageClassifier.load(
+        num_classes=len(classifier.CLASSES),
+        model_path=model_path)
+
+
+load_model()
+
 
 def image_to_annotated_squares(img):
     board = detection.find_board(img)
     squares = detection.split_board(board)
     square_size = squares[0].shape[0]
-
-    model = classifier.ImageClassifier.load(
-        num_classes=len(classifier.CLASSES),
-        model_path=os.path.join(os.path.dirname(__file__), 'trained_non_lin_model_best.pth.tar'))
 
     annotated_board = Image.new('RGBA', (square_size * 8, square_size  * 8))
 
